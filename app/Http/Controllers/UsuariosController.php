29c7df0dd8b9a9ceae7bf996b7c37c1c851eee72
Carloscastro16 <andycastro.001016@gmail.com>
2023-03-07 07:55:34 -0500

    ADD: Funcionalidad de edicion de perfil

--- app/Http/Controllers/UsuariosController.php
+++ app/Http/Controllers/UsuariosController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers;
 use App\Models\Usuarios;
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
 class UsuariosController extends Controller
 {
     public function __construct(){
@@ -20,6 +24,66 @@ class UsuariosController extends Controller
         return view('modulos.MiPerfil');
     }
 
+    public function MiPerfilUpdate(Request $request){
+        //Verificar si el correo actual es diferente al correo Nuevo
+        if(auth()->user()->email != request('email')){
+            //Si se quiere actualizar la contraseña hace esto
+            if(request('passwordN')){
+                $datos = request()->validate([
+                    'name' => ['required', 'string', 'max:255'],
+                    'email' => ['required', 'email', 'unique:users'],
+                    'passwordN' => ['required', 'string', 'min:3']
+                ]);
+            //Sino se quiere actualizar la contraseña
+            }else{
+                $datos = request()->validate([
+                    'name' => ['required', 'string', 'max:255'],
+                    'email' => ['required', 'email', 'unique:users']
+                ]);
+            }
+        //Sino se quiere actualizar el correo
+        }else{
+            if(request('passwordN')){
+                $datos = request()->validate([
+                    'name' => ['required', 'string', 'max:255'],
+                    'email' => ['required', 'email'],
+                    'passwordN' => ['required', 'string', 'min:3']
+                ]);
+            }else{
+                $datos = request()->validate([
+                    'name' => ['required', 'string', 'max:255'],
+                    'email' => ['required', 'email', 'unique:users']
+                ]);
+            }
+        }
+
+        //Si se requiere actualizar el documento
+        if(request('documento')){
+            $documento = request['documento'];
+        }else{
+            $documento = auth()->user()->documento;
+        }
+
+        //Si se quiere actualizar la foto
+        if(request('fotoPerfil')){
+            Storage::delete('public/'.auth()->user()->foto);
+            $rutaImg = $request['fotoPerfil']->store('usuarios/'.$datos["name"], 'public');
+        }else{
+            $rutaImg = auth()->user()->foto;
+        }
+
+        //Si se quiere actualizar la contraseña y cumple con la regla
+        if(isset($datos["passwordN"])){
+            DB::table('users')->where('id', auth()->user()->id)->update(['name'=>$datos["name"],
+            'email'=> $datos["email"], 'documento'=>$documento, 'foto'=>$rutaImg,
+            'password'=>Hash::make(request("psaswordN"))]);
+        }else{
+            DB::table('users')->where('id', auth()->user()->id)->update(['name'=>$datos["name"],
+            'email'=> $datos["email"], 'documento'=>$documento, 'foto'=>$rutaImg]);
+        }
+
+        return redirect('perfil');
+    }
     /**
      * Show the form for creating a new resource.
      *
