e19b92009d9ed2f46c6df77478b516de1e2dbe90
Carloscastro16 <andycastro.001016@gmail.com>
2023-03-13 18:21:46 -0500

    ADD: Cambios, ya listo todo

--- app/Http/Controllers/UsuariosController.php
+++ app/Http/Controllers/UsuariosController.php
@@ -9,86 +9,88 @@ use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
+
+
 class UsuariosController extends Controller
 {
-    public function __construct(){
-        $this->middleware('auth');
+    public function __construct()
+    {
+        //funcion que valida solamente un usario autenticado puede tener acceso
+        $this -> middleware('auth');
+    }
+    //Funcion que se ejecuta cuando se da click en el boton Guardar perfil//
+    public function MiPerfilUpdate(Request $request){
+        //Verificar si el correo actual es diferente al correo enviado
+        //Lo que significa que se requiere actualizar
+        if(auth()-> user()->email !=request('email')){
+            //Si se requiere actualizar la contrasena
+            if(request('passwordN')){
+                //Se crea un array con los datos validados
+                //Si los datos no cumplen las reglas de validacion no se considera para actualizar 
+                $datos = request()-> validate([
+                    'name' => ['required', 'string', 'max:255'],
+                    'email' => ['required', 'email', 'unique:users'],
+                    'passwordN' => ['required', 'string', 'min:3']
+                ]);
+            }else{
+                $datos = request()-> validate([
+                    'name' => ['required', 'string', 'max:255'],
+                    'email' => ['required', 'email', 'unique:users']
+                ]);
+            }
+            //si no se requiere actualizar el correo
+        }else{
+            if(request('paswordN')){
+                $datos = request()-> validate([
+                    'name' => ['required', 'string', 'max:255'],
+                    'email' => ['required', 'email'],
+                    'passwordN' => ['required', 'string', 'min:3']
+                ]);
+            }else{
+                $datos = request()-> validate([
+                    'name' => ['required', 'string', 'max:255'],
+                    'email' => ['required', 'email']
+                ]);
+            }
+        } 
+        //si se requiere actualizar el documento
+        if(request('documento')){
+            $documento = $request['documento'];
+        }else{
+            $documento = auth()->user() -> documento;
+        }
+        //si se requiere actualizar la foto
+        if(request('fotoPerfil')){
+            Storage::delete('public/'.auth()->user()-> foto);
+            $rutaImg = $request['fotoPerfil']->store('usuarios/'.$datos["name"],'public');
+        }else{
+            $rutaImg = auth()->user() ->foto;
+        }
+        //si se requiere actualizar la contrasena y cumple con las reglas
+        if(isset($datos["passwordN"])){
+            DB::table('users') -> where ('id',auth() -> user()-> id) ->update(['name' => $datos ["name"],
+            'email' => $datos["email"],'documento'=>$documento, 'foto' =>$rutaImg,
+            'password' => Hash::make(request("passwordN"))]);
+        }else{
+            DB::table('users') -> where ('id',auth() -> user()-> id) ->update(['name' => $datos ["name"],
+            'email' => $datos["email"],'documento'=>$documento, 'foto' =>$rutaImg]);
+        }
+        return redirect('MiPerfil');
+    }
+    public function Miperfil(){
+        return view ('modulos.MiPerfil');
     }
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function MiPerfil()
+    public function index()
     {
-        return view('modulos.MiPerfil');
+        $usuarios = Usuarios::all();
+        return view('modulos.Usuarios')-> with ('usuarios',$usuarios);
     }
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function MiPerfilUpdate(Request $request){
-        //Verificar si el correo actual es diferente al correo Nuevo
-        if(auth()->user()->email != request('email')){
-            //Si se quiere actualizar la contraseña hace esto
-            if(request('passwordN')){
-                $datos = request()->validate([
-                    'name' => ['required', 'string', 'max:255'],
-                    'email' => ['required', 'email', 'unique:users'],
-                    'passwordN' => ['required', 'string', 'min:3']
-                ]);
-            //Sino se quiere actualizar la contraseña
-            }else{
-                $datos = request()->validate([
-                    'name' => ['required', 'string', 'max:255'],
-                    'email' => ['required', 'email', 'unique:users']
-                ]);
-            }
-        //Sino se quiere actualizar el correo
-        }else{
-            if(request('passwordN')){
-                $datos = request()->validate([
-                    'name' => ['required', 'string', 'max:255'],
-                    'email' => ['required', 'email'],
-                    'passwordN' => ['required', 'string', 'min:3']
-                ]);
-            }else{
-                $datos = request()->validate([
-                    'name' => ['required', 'string', 'max:255'],
-                    'email' => ['required', 'email', 'unique:users']
-                ]);
-            }
-        }
 
-        //Si se requiere actualizar el documento
-        if(request('documento')){
-            $documento = request['documento'];
-        }else{
-            $documento = auth()->user()->documento;
-        }
-
-        //Si se quiere actualizar la foto
-        if(request('fotoPerfil')){
-            Storage::delete('public/'.auth()->user()->foto);
-            $rutaImg = $request['fotoPerfil']->store('usuarios/'.$datos["name"], 'public');
-        }else{
-            $rutaImg = auth()->user()->foto;
-        }
-
-        //Si se quiere actualizar la contraseña y cumple con la regla
-        if(isset($datos["passwordN"])){
-            DB::table('users')->where('id', auth()->user()->id)->update(['name'=>$datos["name"],
-            'email'=> $datos["email"], 'documento'=>$documento, 'foto'=>$rutaImg,
-            'password'=>Hash::make(request("psaswordN"))]);
-        }else{
-            DB::table('users')->where('id', auth()->user()->id)->update(['name'=>$datos["name"],
-            'email'=> $datos["email"], 'documento'=>$documento, 'foto'=>$rutaImg]);
-        }
-
-        return redirect('MiPerfil');
-    }
     /**
      * Show the form for creating a new resource.
      *
@@ -107,7 +109,24 @@ class UsuariosController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        //Validar los datos recibidos
+        $datos = request()-> validate([
+            'name' => ['string', 'max:255'],
+            'rol' => ['required'],
+            'email' => ['string', 'unique:users'],
+            'password' => ['string', 'min:3']
+        ]);
+        //crear el registro en la tabla users en la bd
+        Usuarios::create([
+            'name' => $datos['name'],
+            'email' => $datos['email'],
+            'rol' => $datos['rol'],
+            'password' => Hash::make($datos['password']),
+            'documento' => '',
+            'foto' => ''
+        ]);
+        //redireccionamos a la vista usuarios, al llamar a la ruta Usuarios
+return redirect('Usuarios') -> with('UsuarioCreado','OK');
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,7 @@ Auth::routes();
 Route::get('Inicio', [InicioController::class, 'index']);
 Route::get('/MiPerfil', [App\Http\Controllers\UsuariosController::class, 'MiPerfil'])->name('MiPerfil');
 Route::put('/MiPerfil', [App\Http\Controllers\UsuariosController::class, 'MiPerfilUpdate'])->name('MiPerfilUpdate');
+Route::get('/Usuarios', [App\Http\Controllers\UsuariosController::class, 'index'])->name('index');
 
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
